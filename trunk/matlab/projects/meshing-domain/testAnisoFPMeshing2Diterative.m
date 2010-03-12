b7e3400c4f57696eef3d68bc5e6bcf9f09b109d0
gabriel.peyre <gabriel.peyre@a6734f8e-26ab-11df-90c7-81228a7a91d5>
2010-03-12 21:34:25 +0000

    Code meshing domain

--- trunk/matlab/projects/meshing-domain/testAnisoFPMeshing2Diterative.m
+++ trunk/matlab/projects/meshing-domain/testAnisoFPMeshing2Diterative.m
@@ -1,17 +1,18 @@
-clear all; close all; clc;
+% clear all; close all; clc;
 %--------------------------------------------------------------------------
-library_directory = '../mex';
-addpath(library_directory);
-matlab_directory   = '../matlab/';
-addpath(matlab_directory);
+add_base_paths;
+
 %%
 % load mask
 P = imread('sala.png');
 n = size(P,1);
 m = size(P,2);
 
-SE=strel('square',3);
-P = imopen(P,SE);    % IMPORTANT : le bord du domaine doit pouvoir s'extraire en 4-conexité
+% SE = strel('square',3);
+% P = imopen(P,SE);    % IMPORTANT : le bord du domaine doit pouvoir s'extraire en 4-conexité
+
+P = conv2(double(P), ones(3), 'same')>0;
+% P = conv2(double(P==0), ones(3), 'same')==0;
 
 % pick points
 S = zeros(n,m);
@@ -26,7 +27,7 @@ T(:,:,1,1) = 1;
 T(:,:,2,2) = 1;
 
 % sampling
-nb_seeds = 100;
+nb_seeds = 300;
 tic
 [U,V,dUx,dUy] = anisoFPMeshing2Diterative(T,S,nb_seeds);   % 5th USE (with mask implicitely in S)
 toc
@@ -43,15 +44,29 @@ for i=1:n
         end
     end
 end
+
+% convert voronoi to color
+[a,b,W] = unique(V(:));
+W = reshape(W, size(V));
+c = jet(256);
+I = floor(255*rescale(W))+1;
+Z = c(I(:),:); Z = reshape(Z, [size(W) 3]);
+I = find(P==0);
+Z([I I+prod(size(P)) I+2*prod(size(P))]) = 1;
+
+imageplot(Z);
+colormap jet(256);
+hold on;
+plot_pts(sp,'k.',20);
+hold off;
+
+
+return;
+
 imagesc(U);
 axis image; axis off;
 colormap jet(256);
 hold on;
 plot_pts(sp, 'r.',10);
 hold off;
-figure;
-imagesc(V);
-axis image; axis off;
-hold on;
-plot_pts(sp,'k.',20);
-hold off;
+
