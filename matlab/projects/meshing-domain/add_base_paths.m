ad0cd8b746bff5fb2ea242cdb148fcdc40e6ba0c
gabriel.peyre <gabriel.peyre@a6734f8e-26ab-11df-90c7-81228a7a91d5>
2010-03-13 02:38:35 +0000

    Synthese de metrique pour shapes

---
+++ matlab/projects/meshing-domain/add_base_paths.m
@@ -0,0 +1,6 @@
+library_directory = '../../mex';
+addpath(library_directory);
+matlab_directory   = '../../toolbox/';
+addpath(matlab_directory);
+images_directory   = '../../data/images/';
+addpath(images_directory);
